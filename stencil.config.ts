785c495ee32a964f9edf5decc6e775780df8054b
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-03-30 15:29:44 +0700

    pasang postcss, autoprefixer, & tailwind

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,7 @@
 import { Config } from '@stencil/core';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+import tailwind from 'tailwindcss';
 
 export const config: Config = {
   namespace: 'kompas-web-components',
@@ -12,10 +15,19 @@ export const config: Config = {
     },
     {
       type: 'docs-readme',
+      footer: '*Terbikin oleh tim front-end kompas.id*'
     },
     {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
   ],
+  plugins: [
+    postcss({
+      plugins: [
+        autoprefixer(),
+        tailwind()
+      ]
+    })
+  ]
 };
