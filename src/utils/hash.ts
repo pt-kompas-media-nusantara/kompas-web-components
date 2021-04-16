9cca6f5eb9d0ac4c6f8d136a87df65ff5d1cb9bb
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-04-16 08:57:06 +0700

    Create hash.ts

---
+++ src/utils/hash.ts
@@ -0,0 +1,19 @@
+import { encode } from 'base-64'
+
+const base64Encode = (str:string): string => {
+  /**
+   * Ini karena di node.js belum ada btoa() sehingga bikin gagal tes
+   */
+  if (!global.btoa) {
+    global.btoa = encode;
+  }
+  // const utf8Bytes = encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (_match, p1:string) {
+  //   return String.fromCharCode('0x' + p1);
+  // })
+
+  return btoa(str);
+}
+
+export {
+  base64Encode
+}
