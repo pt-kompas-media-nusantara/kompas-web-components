ecc16e5c1953fcb13a8af8f74f9b0d07902a7991
Adi <adiandoro58@gmail.com>
2022-04-06 17:11:41 +0700

    + Fix styling

--- src/components/kid-metered-paywall/kid-metered-paywall.tsx
+++ src/components/kid-metered-paywall/kid-metered-paywall.tsx
@@ -36,7 +36,7 @@ export class KidMeteredPaywall {
    */
   private templateMeteredPaywall() {
     return (
-      <div class="flex flex-row w-full bg-blue-100 py-4 justify-center space-x-4 px-4 lg:px-0 fixed bottom-0">
+      <div class="flex flex-row w-full bg-blue-100 py-4 justify-center space-x-4 px-4 lg:px-0 bottom-0">
         <div class="text-grey-600 text-sm md:text-lg self-center text-left">
           {this.getCountdownArticle()}
         </div>
@@ -53,7 +53,7 @@ export class KidMeteredPaywall {
   private getCountdownArticle(): void {
     const getCountdown = this.countdownArticle
     if (getCountdown < 5) {
-      return <p>Anda memiliki sisa <b> {this.countdownArticle} dari 5</b> artikel premium gratis bulan ini.Langganan untuk akses tanpa batas</p>
+      return <p>Anda memiliki sisa <b> {this.countdownArticle} dari 5</b> artikel premium gratis bulan ini. Langganan untuk akses tanpa batas</p>
     } else {
       return <p>Anda sedang membaca artikel gratis terakhir bulan ini. Langganan untuk akses tanpa batas</p>
     }
@@ -61,7 +61,7 @@ export class KidMeteredPaywall {
 
   render() {
     return (
-      <div class="fixed w-full h-full">
+      <div class=" sticky bottom-0 w-full h-full">
         {this.countdownArticle > 0 && this.countdownArticle < 6 && this.isLogin ? this.templateMeteredPaywall() : ''}
       </div>
     )
