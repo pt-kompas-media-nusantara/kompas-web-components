f8a5ccb390cecdf94a02984696b3bb2c484c4edb
adiandroo <adiandoro58@gmail.com>
2022-10-28 17:59:23 +0700

    ! Fix endpoint

--- src/components/kompas-paywall-body/kompas-paywall-body.tsx
+++ src/components/kompas-paywall-body/kompas-paywall-body.tsx
@@ -21,7 +21,7 @@ export class KompasPaywallBody {
   @State() isExtensionsOpened: boolean = false
   @State() kompasAkunHost: string = 'https://akun.kompas.id'
   @State() kompasApigenHost: string = 'https://apigen.kompas.id'
-  @State() kompasApiWcmHost: string = 'https://apiwcm.kompas.id'
+  @State() kompasApiWcmHost: string = 'https://apiwcm.kompas.cloud'
   @State() kompasLoginHost: string = 'https://account.kompas.cloud'
   @State() selfHost: string = 'https://epaper.kompas.cloud'
   @State() swgPublisherName: string = 'Harian Kompas Dev'
