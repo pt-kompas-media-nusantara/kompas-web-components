5fa237e70bb0fddc028711c83000399d0a5cd024
nikocean <nikolasmarcellino8@gmail.com>
2023-02-14 21:18:21 +0700

    hide cart and transaction menu

--- src/components/kompas-header-account-menu/kompas-header-account-menu.tsx
+++ src/components/kompas-header-account-menu/kompas-header-account-menu.tsx
@@ -1,7 +1,7 @@
 import { Component, h, Prop } from '@stencil/core'
-import shoppingCart from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/shopping-cart.svg'
+// import shoppingCart from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/shopping-cart.svg'
 import userCog from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/user-cog.svg'
-import exchangeAlt from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/exchange-alt.svg'
+// import exchangeAlt from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/exchange-alt.svg'
 import bell from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/bell.svg'
 import bookmark from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/bookmark.svg'
 import signOutAlt from '../../../assets/fontawesome-free-5.15.3-web/svgs/solid/sign-out-alt.svg'
@@ -68,20 +68,20 @@ export class KompasHeaderAccountMenu {
       <div class="border-b border-grey-300 text-grey-600">
         <li class="font-bold flex flex-shrink-0 flex-col text-left text-sm mb-4">
           {/* Cart */}
-          <a href={this.cartUrl} class="header-account-menu--item">
+          {/* <a href={this.cartUrl} class="header-account-menu--item">
             <div class="icon-md icon-blue-600" innerHTML={shoppingCart}></div>
             <p class="header-account-menu--title">Keranjang</p>
-          </a>
+          </a> */}
           {/* Manage Account  */}
           <a href={this.manageAccountUrl} class="header-account-menu--item">
             <div class="icon-md icon-blue-600" innerHTML={userCog}></div>
             <p class="header-account-menu--title">Kelola Akun</p>
           </a>
           {/* Transaction  */}
-          <a href={this.ordersUrl} class="header-account-menu--item">
+          {/* <a href={this.ordersUrl} class="header-account-menu--item">
             <div class="icon-md icon-blue-600" innerHTML={exchangeAlt}></div>
             <p class="header-account-menu--title">Transaksi</p>
-          </a>
+          </a> */}
           {/* Notifications  */}
           <a href={this.notificationUrl} class="header-account-menu--item">
             <div class="icon-md icon-blue-600" innerHTML={bell}></div>
