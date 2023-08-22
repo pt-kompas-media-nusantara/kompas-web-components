91bd2e2efbc268da3ceffb0d79532c4b3245e59a
bayu <bayusetiawanipb@gmail.com>
2023-08-22 11:14:48 +0700

    add dynamic color transition box paywall

--- src/components/kompas-paywall/kompas-paywall.tsx
+++ src/components/kompas-paywall/kompas-paywall.tsx
@@ -172,7 +172,7 @@ export class KompasPaywall {
 
 
   private transitionBox = (): void => (
-    <div class="transparent-linear h-20 -mt-24 z-0 w-full " />
+    <div class={`h-20 ${this.theme === 'dark' ? 'transparent-linear-dark' : 'transparent-linear'} -mt-24 z-0 w-full `} />
   )
 
   private renderEpaperPaywallSection = (): void => (
@@ -185,7 +185,7 @@ export class KompasPaywall {
       return (
         <div>
           {this.transitionBox()}
-          <div class="flex flex-col bg-white items-center justify-center mx-4 md:mx-0">
+          <div class={`flex flex-col ${this.theme === 'dark' ? 'bg-dark-5' : 'bg-white'} items-center justify-center mx-4 md:mx-0`}>
             <div class="flex flex-col w-full max-w-screen-md my-5">
               <kompas-paywall-information-header text={this.textHeader || defaultHeaderText} theme={this.theme}></kompas-paywall-information-header>
               <kompas-paywall-body paywall_location={this.paywall_location} paywall_subscription_package={this.paywall_subscription_package} paywall_subscription_id={this.paywall_subscription_id} paywall_subscription_price={this.paywall_subscription_price} paywall_position={this.paywall_position} tracker_page_type={this.tracker_page_type} tracker_content_id={this.tracker_content_id} tracker_content_title={this.tracker_content_title} tracker_content_categories={this.tracker_content_categories} tracker_user_type={this.tracker_user_type} tracker_subscription_status={this.tracker_subscription_status} tracker_page_domain={this.tracker_page_domain} tracker_metered_wall_type={this.tracker_metered_wall_type} tracker_epaper_edition={this.tracker_epaper_edition} tracker_metered_wall_balance={this.tracker_metered_wall_balance} tracker_content_type={this.tracker_content_type} swgEnable={this.swgEnable} isLogin={this.isLogin} type={this.type} countdownArticle={this.countdownArticle} paywallData={this.paywallData} subscriptionStatus={this.subscriptionStatus} userGuid={this.userGuid} theme={this.theme}></kompas-paywall-body>
@@ -197,7 +197,7 @@ export class KompasPaywall {
       return (
         <div>
           {this.transitionBox()}
-          <div class="flex flex-col bg-white items-center justify-center mx-4 md:mx-0">
+          <div class={`flex flex-col ${this.theme === 'dark' ? 'bg-dark-5' : 'bg-white'} items-center justify-center mx-4 md:mx-0`}>
             <div class="flex flex-col w-full max-w-screen-md my-5">
               <kompas-paywall-banner-registration bannerData={this.paywallData.informations.register} theme={this.theme}></kompas-paywall-banner-registration>
               <kompas-paywall-body tracker_content_type={this.tracker_content_type} paywall_location={this.paywall_location} paywall_subscription_package={this.paywall_subscription_package} paywall_subscription_id={this.paywall_subscription_id} paywall_subscription_price={this.paywall_subscription_price} paywall_position={this.paywall_position} tracker_page_type={this.tracker_page_type} tracker_content_id={this.tracker_content_id} tracker_content_title={this.tracker_content_title} tracker_content_categories={this.tracker_content_categories} tracker_user_type={this.tracker_user_type} tracker_subscription_status={this.tracker_subscription_status} tracker_epaper_edition={this.tracker_epaper_edition} tracker_page_domain={this.tracker_page_domain} tracker_metered_wall_type={this.tracker_metered_wall_type} tracker_metered_wall_balance={this.tracker_metered_wall_balance} swgEnable={this.swgEnable} isLogin={this.isLogin} type={this.type} countdownArticle={this.countdownArticle} paywallData={this.paywallData} subscriptionStatus={this.subscriptionStatus} userGuid={this.userGuid} theme={this.theme}></kompas-paywall-body>
