48a76b4337d2aa62ba7379c321f9f83e7acce830
Billy Surya <billy.surya@kompas.com>
2023-08-24 18:19:18 +0700

    feat(kompas-metered-register): handle banner content based on article count

--- src/components/kompas-metered-register/kompas-metered-register.tsx
+++ src/components/kompas-metered-register/kompas-metered-register.tsx
@@ -16,48 +16,56 @@ export class KompasMeteredRegister {
   @State() registerUrl: string = 'https://account.kompas.cloud/register';
   @State() isShowBanner: boolean = true;
   @State() isExpandBanner: boolean = false;
-
   /**
    * prop countdownArticle untuk menghandle sudah berapa artikel gratis yang user baca.
    */
   @Prop() countdownArticle: number = 0;
 
   /**
-   * mengelola tampilan pada section metered paywall
+   * mengelola tampilan pada section metered register
    */
   private bannerContent(): void {
     if (!this.isExpandBanner) {
       return (
-        <p class="text-base md:text-lg font-lora">
-          Ini Adalah <b>Artikel Gratis Terakhir</b> Anda. <b>Daftar Akun untuk Terus Membaca.</b>
-        </p>
+        <Fragment>
+          <div class="text-base md:text-lg text-center md:text-left font-lora mb-3 md:mb-0 px-8 md:px-0">
+            {this.countdownArticle > 1 ? (
+              <b>Dukung Jurnalisme Berkualitas dengan Mendaftar Akun Kompas.id.</b>
+            ) : (
+              <span>
+                Ini Adalah <b>Artikel Gratis Terakhir</b> Anda. <b>Daftar Akun untuk Terus Membaca.</b>
+              </span>
+            )}
+          </div>
+          <div class="md:self-center">{this.registerButtonTemplate()}</div>
+        </Fragment>
       );
     } else {
       return (
         <Fragment>
           <div class="flex flex-col-reverse md:flex-row justify-center gap-8">
-            <div class="text-center md:text-left md:w-1/2">
+            <div class="flex flex-col justify-evenly text-center md:text-left md:w-5/12 gap-2">
               <p class="text-lg md:text-2xl font-lora">
-                Anda Sedang Membaca{' '}
-                <b>
-                  Artikel Premium <br />
-                  Gratis Terakhir
-                </b>{' '}
-                dari Kompas.id
+                {this.countdownArticle > 1 ? (
+                  <b>Tertarik dengan Artikel Ini? Daftar untuk Akses Artikel Menarik Lainnya</b>
+                ) : (
+                  <span>
+                    Anda Sedang Membaca <b>Artikel Premium Gratis Terakhir</b> dari Kompas.id
+                  </span>
+                )}
               </p>
-              <p class="text-sm md:text-base my-3">
-                Ayo daftar akun untuk akses ke beragam artikel dan fitur premium. Anda juga mendukung jurnalisme berkualitas dengan mendaftar akun.
+              <p class="text-sm md:text-base">
+                {this.countdownArticle > 1 ? (
+                  <Fragment>Dapatkan akses ke beragam konten dan fitur premium Kompas.id.</Fragment>
+                ) : (
+                  <Fragment>Ayo daftar akun untuk akses ke beragam artikel dan fitur premium.</Fragment>
+                )}{' '}
+                Anda juga mendukung jurnalisme berkualitas dengan mendaftar akun.
               </p>
-              <div class="md:self-start">
-                <button onClick={this.redirectToRegister} class="bg-green-500 p-2 w-full md:w-auto rounded-md font-bold text-grey-100 px-6 text-sm md:text-base">
-                  Daftar Akun
-                </button>
-              </div>
+              <div class="md:self-start">{this.registerButtonTemplate()}</div>
             </div>
             <div class="flex justify-center">
-              <img src="https://d3w4qaq4xm1ncv.cloudfront.net/paywall-asset/paywall_ilustrasi3-03_1.png" class="h-40 w-40 md:w-full md:h-full">
-                {' '}
-              </img>
+              <img src="https://d3w4qaq4xm1ncv.cloudfront.net/paywall-asset/paywall_ilustrasi3-03_1.png" class="h-40 w-40 md:w-full md:h-full" />
             </div>
           </div>
         </Fragment>
@@ -65,10 +73,21 @@ export class KompasMeteredRegister {
     }
   }
 
+  /**
+   * template button register
+   */
+  private registerButtonTemplate = (): void => {
+    return (
+      <button onClick={this.redirectToRegister} class="bg-green-500 p-1.5 w-full md:w-auto rounded-md font-bold text-grey-100 px-5 text-sm md:text-base">
+        Daftar Akun
+      </button>
+    );
+  };
+
   private bannerTemplate = (): void => {
     return (
       <Fragment>
-        <div class="text-grey-600 text-sm md:text-lg self-center text-left ml-auto">{this.bannerContent()}</div>
+        <div class="block md:flex items-center justify-center gap-8 text-grey-600 text-sm md:text-lg self-center text-left ml-auto">{this.bannerContent()}</div>
       </Fragment>
     );
   };
@@ -94,12 +113,12 @@ export class KompasMeteredRegister {
       <Host>
         {this.isShowBanner ? (
           <div class="sticky bottom-0 w-full h-full">
-            <div class={`w-full bg-blue-100 lg:px-0 bottom-0 ${this.isExpandBanner ? 'p-8' : 'p-4'}`}>
-              <div class="flex max-w-7xl m-auto relative justify-center">
+            <div class={`w-full bg-blue-100 px-4 lg:px-0 bottom-0 py-4`}>
+              <div class="flex max-w-7xl m-auto justify-center relative">
                 <div class="flex flex-col">{this.bannerTemplate()}</div>
-                <div class="absolute right-0 top-0">
-                  <button onClick={this.triggerExpandBanner} class="bg--gray p-2 rounded-md ">
-                    <div class="icon icon-brand-1" innerHTML={chevronUp}></div>
+                <div class="absolute right-0 top-0.5">
+                  <button onClick={this.triggerExpandBanner} class="bg--gray p-2.5 rounded-md ">
+                    <div class={`icon icon-brand-1 ${this.isExpandBanner && 'chevron-down'}`} innerHTML={chevronUp}></div>
                   </button>
                 </div>
               </div>
